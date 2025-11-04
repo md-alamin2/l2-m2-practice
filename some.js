906bc2340fb77c2cbf0d55f5164b169b1a8e7b57
md-alamin2 <mdalamin22671@gmail.com>
2025-11-04 22:12:47 +0600

    learn about array.from()

--- some.js
+++ some.js
@@ -2,7 +2,7 @@
 
 const arr = [1, 2, 3, 4, 5];
 
-const hasEvenNumber = arr.some((num)=> !num % 2 === 0);
+const hasEvenNumber = arr.some((num) => !num % 2 === 0);
 
 // console.log(hasEvenNumber)
 
@@ -10,11 +10,22 @@ const currentUserRoles = ["user", "editor"];
 
 const featureAccessRoles = ["admin", "manager"];
 
-const hasAccess = currentUserRoles.some(role => featureAccessRoles.includes(role))
+const hasAccess = currentUserRoles.some((role) =>
+  featureAccessRoles.includes(role)
+);
 
 // console.log(hasAccess)
 
-
 // ? Array from() method
+
+// const arr2 = Array.from([1, 2, 3, 4, 5], (num, i) => num * i);
+
+// console.log(arr2);
+
+const range = (start, stop, step)=>{
+  return Array.from({length: Math.ceil((stop - start)/step)}, (_, i)=> start + i * step);
+}
+
+console.log(range(1, 11, 1))
 
 
