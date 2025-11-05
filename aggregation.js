d2f37f677cebf2d4237cebbe07def29cec439a1c
md-alamin2 <mdalamin22671@gmail.com>
2025-11-05 22:38:38 +0600

    learn more about aggregation

--- aggregation.js
+++ aggregation.js
@@ -26,11 +26,11 @@ const surveyResponses = [
 // TODO: if not exist then initialize with count 1
 
 const count = surveyResponses.reduce((table, response) => {
-  //   if (table[response]) {
-  //     table[response] = table[response] + 1;
-  //   } else {
-  //     table[response] = 1;
-  //   }
+    if (table[response]) {
+      table[response] = table[response] + 1;
+    } else {
+      table[response] = 1;
+    }
 
   //   one liner of the above code
   //   table[response] = (table[response] || 0) + 1;
@@ -41,7 +41,54 @@ const count = surveyResponses.reduce((table, response) => {
   return table;
 }, {});
 
-console.log(count);
+// console.log(count);
 
 //? Output
 // { A: 5, C: 3, B: 5, D: 1 }
+
+// Scenario: You have a flat array of sales data, and you need to group the sales by category,
+// calculating the total revenue and the number of items sold for each.
+
+const sales = [
+  { category: "Electronics", item: "Laptop", price: 1200, quantity: 1 },
+  { category: "Books", item: "JS Basics", price: 30, quantity: 2 },
+  { category: "Electronics", item: "Mouse", price: 25, quantity: 2 },
+  { category: "Home", item: "Chair", price: 150, quantity: 1 },
+  { category: "Books", item: "React Deep Dive", price: 50, quantity: 1 },
+  { category: "Electronics", item: "Keyboard", price: 80, quantity: 1 },
+];
+
+// TODO initial an empty object
+// TODO initial an object of category
+// TODO calculate the revenue
+
+const salesTable = sales.reduce((table, sale) => {
+  const { category, price, quantity } = sale;
+  if (!table[category]) {
+    table[category] = {
+      totalRevenue: 0,
+      itemCount: 0,
+    };
+  }
+
+  table[category].totalRevenue += price * quantity;
+  table[category].itemCount += quantity;
+  return table
+}, {});
+console.log(salesTable)
+
+//? Output
+// {
+//   Electronics: {
+//     totalRevenue: 1330,
+//     itemCount: 4,
+//   },
+//   Books: {
+//     totalRevenue: 110,
+//     itemCount: 3,
+//   },
+//   Home: {
+//     totalRevenue: 150,
+//     itemCount: 1,
+//   },
+// };
